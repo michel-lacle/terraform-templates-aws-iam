a3e9d0b2ec944af08fca03294fcda928757672f2
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-17 10:49:17 -0400

    adding devs to the developers group

--- aws_iam_group.tf
+++ aws_iam_group.tf
@@ -2,3 +2,15 @@ resource "aws_iam_group" "developers" {
   name = "developers"
   path = "/users/"
 }
+
+# now add all developers to the developers group
+resource "aws_iam_group_membership" "developers_group_membership" {
+  name = "developers-group-membership"
+
+  users = [
+    aws_iam_user.developer1.name,
+    aws_iam_user.developer2.name,
+  ]
+
+  group = aws_iam_group.developers.name
+}
--- aws_iam_user.tf
+++ aws_iam_user.tf
@@ -2,3 +2,8 @@ resource "aws_iam_user" "developer1" {
   name = "developer1"
   path = "/users/"
 }
+
+resource "aws_iam_user" "developer2" {
+  name = "developer2"
+  path = "/users/"
+}
